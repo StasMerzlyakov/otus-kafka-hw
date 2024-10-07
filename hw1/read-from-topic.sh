645abef9fb338cb615f37df0d43e0471c3874b3e
Stas Merzlyakov <merzliakov@gmail.com>
2024-10-07 15:29:31 +0400

    добавил в комментарии команду для получения информации о топике + экспериментировал с group id

--- hw1/read-from-topic.sh
+++ hw1/read-from-topic.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# list topics
+#kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092 # --describe
+
+
+# --group 1
 kafka/bin/kafka-console-consumer.sh --topic test --from-beginning --bootstrap-server localhost:9092
 
